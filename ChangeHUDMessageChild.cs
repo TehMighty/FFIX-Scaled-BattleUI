8e1190539f8dd7249a19554acc5a9265ba67816d
TehMighty <jramsay16@gmail.com>
2018-07-29 17:35:23 -0300

    Upload All
    Uploading All.

---
+++ ChangeHUDMessageChild.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using UnityEngine;
+
+// Token: 0x020003E9 RID: 1001
+[RequireComponent (typeof (UILabel))]
+[RequireComponent (typeof (TweenPosition))]
+[RequireComponent (typeof (TweenAlpha))]
+public class HUDMessageChild : MonoBehaviour {
+	// Token: 0x170002DC RID: 732
+	// (get) Token: 0x060019C3 RID: 6595 RVA: 0x0002481B File Offset: 0x00022A1B
+	public HUDMessage.MessageStyle DisplayStyle {
+		get {
+			return this.displayStyle;
+		}
+	}
+
+	// Token: 0x170002DD RID: 733
+	// (get) Token: 0x060019C4 RID: 6596 RVA: 0x00024823 File Offset: 0x00022A23
+	public UIFollowTarget Follower {
+		get {
+			return this.follower;
+		}
+	}
+
+	// Token: 0x170002DE RID: 734
+	// (get) Token: 0x060019C5 RID: 6597 RVA: 0x0002482B File Offset: 0x00022A2B
+	// (set) Token: 0x060019C6 RID: 6598 RVA: 0x00024833 File Offset: 0x00022A33
+	public byte MessageId {
+		get {
+			return this.messageId;
+		}
+		set {
+			this.messageId = value;
+		}
+	}
+
+	// Token: 0x170002DF RID: 735
+	// (get) Token: 0x060019C7 RID: 6599 RVA: 0x0002483C File Offset: 0x00022A3C
+	public float Countdown {
+		get {
+			return this.countdown;
+		}
+	}
+
+	// Token: 0x170002E0 RID: 736
+	// (set) Token: 0x060019C8 RID: 6600 RVA: 0x00024844 File Offset: 0x00022A44
+	public string Label {
+		set {
+			this.label.text = value;
+		}
+	}
+
+	// Token: 0x170002E1 RID: 737
+	// (get) Token: 0x060019C9 RID: 6601 RVA: 0x00024852 File Offset: 0x00022A52
+	public GameObject ParentGameObject {
+		get {
+			return this.parentGameObject;
+		}
+	}
+
+	// Token: 0x060019CA RID: 6602 RVA: 0x0017E960 File Offset: 0x0017CB60
+	public void Initial () {
+		bool flag = !this.isInitialized;
+		if (flag) {
+			this.myTransform = base.transform;
+			this.myTransform.localScale = Vector2.one * 1.45f; // TehMight
+			this.parentGameObject = this.myTransform.parent.gameObject;
+			this.label = base.GetComponent<UILabel> ();
+			this.uiWidget = base.GetComponent<UIWidget> ();
+			this.tweenPosition = base.GetComponent<TweenPosition> ();
+			this.tweenAlpha = base.GetComponent<TweenAlpha> ();
+			this.follower = this.myTransform.parent.GetComponent<UIFollowTarget> ();
+			this.follower.enabled = false;
+			this.tweenPositionDuration = this.tweenPosition.duration;
+			this.tweenAlphaDuration = this.tweenAlpha.duration;
+			this.tweenAlpha.animationCurve = Singleton<HUDMessage>.Instance.alphaTweenCurve;
+			this.isInitialized = true;
+		}
+	}
+
+	// Token: 0x060019CB RID: 6603 RVA: 0x0002485A File Offset: 0x00022A5A
+	private void EnableTween (bool isEnabled) {
+		this.tweenPosition.enabled = isEnabled;
+		this.tweenAlpha.enabled = isEnabled;
+	}
+
+	// Token: 0x060019CC RID: 6604 RVA: 0x0017EA54 File Offset: 0x0017CC54
+	public void Show (Transform target, string message, HUDMessage.MessageStyle style, Vector3 offset) {
+		this.displayStyle = style;
+		this.follower.target = target;
+		this.follower.targetTransformOffset = offset;
+		this.label.text = message;
+		this.tweenPosition.duration = this.tweenPositionDuration / Singleton<HUDMessage>.Instance.Speed;
+		this.tweenAlpha.duration = this.tweenAlphaDuration / Singleton<HUDMessage>.Instance.Speed;
+		switch (style) {
+			case HUDMessage.MessageStyle.DAMAGE:
+			case HUDMessage.MessageStyle.GUARD:
+			case HUDMessage.MessageStyle.MISS:
+			case HUDMessage.MessageStyle.DEATH:
+				this.DamageSetting ();
+				break;
+			case HUDMessage.MessageStyle.RESTORE_HP:
+			case HUDMessage.MessageStyle.RESTORE_MP:
+				this.RestoreSetting ();
+				break;
+			case HUDMessage.MessageStyle.DEATH_SENTENCE:
+				this.DeathSentencesSetting ();
+				break;
+			case HUDMessage.MessageStyle.PETRIFY:
+				this.PetrifySetting ();
+				break;
+			case HUDMessage.MessageStyle.CRITICAL:
+				this.CriticalSetting ();
+				break;
+		}
+		this.PlayAnimation ();
+		this.PrintLog (true);
+	}
+
+	// Token: 0x060019CD RID: 6605 RVA: 0x0017EB3C File Offset: 0x0017CD3C
+	private void DamageSetting () {
+		this.uiWidget.color = Singleton<HUDMessage>.Instance.damageColor;
+		this.tweenPosition.animationCurve = Singleton<HUDMessage>.Instance.damageTweenCurve;
+		this.tweenPosition.to = HUDMessage.NormalTargetPosition;
+		this.EnableTween (true);
+	}
+
+	// Token: 0x060019CE RID: 6606 RVA: 0x0017EB8C File Offset: 0x0017CD8C
+	private void CriticalSetting () {
+		this.uiWidget.color = Singleton<HUDMessage>.Instance.criticalColor;
+		this.tweenPosition.animationCurve = Singleton<HUDMessage>.Instance.criticalTweenCurve;
+		this.tweenPosition.to = HUDMessage.NormalTargetPosition;
+		this.EnableTween (true);
+	}
+
+	// Token: 0x060019CF RID: 6607 RVA: 0x0017EBDC File Offset: 0x0017CDDC
+	private void RestoreSetting () {
+		this.label.text = NGUIText.EncodeColor (this.label.text, Singleton<HUDMessage>.Instance.restoreColor);
+		this.tweenPosition.animationCurve = Singleton<HUDMessage>.Instance.restoreTweenCurve;
+		this.tweenPosition.to = HUDMessage.RecoverTargetPosition;
+		this.EnableTween (true);
+	}
+
+	// Token: 0x060019D0 RID: 6608 RVA: 0x00024874 File Offset: 0x00022A74
+	private void DeathSentencesSetting () {
+		this.uiWidget.color = Singleton<HUDMessage>.Instance.damageColor;
+		this.EnableTween (false);
+	}
+
+	// Token: 0x060019D1 RID: 6609 RVA: 0x00024874 File Offset: 0x00022A74
+	private void PetrifySetting () {
+		this.uiWidget.color = Singleton<HUDMessage>.Instance.damageColor;
+		this.EnableTween (false);
+	}
+
+	// Token: 0x060019D2 RID: 6610 RVA: 0x00024892 File Offset: 0x00022A92
+	private void PlayAnimation () {
+		this.parentGameObject.SetActive (true);
+	}
+
+	// Token: 0x060019D3 RID: 6611 RVA: 0x00016C5C File Offset: 0x00014E5C
+	private void PrintLog (bool isShow) { }
+
+	// Token: 0x060019D4 RID: 6612 RVA: 0x000248A0 File Offset: 0x00022AA0
+	public void SetupCamera (Camera worldCamera, Camera uiCamera) {
+		this.follower.worldCam = worldCamera;
+		this.follower.uiCam = uiCamera;
+	}
+
+	// Token: 0x060019D5 RID: 6613 RVA: 0x0017EC3C File Offset: 0x0017CE3C
+	public void Clear () {
+		if (this.isInitialized) {
+			this.PrintLog (false);
+			this.parentGameObject.SetActive (false);
+			this.EnableTween (false);
+			this.tweenPosition.duration = this.tweenPositionDuration;
+			this.tweenAlpha.duration = this.tweenAlphaDuration;
+			this.tweenPosition.ResetToBeginning ();
+			this.tweenAlpha.ResetToBeginning ();
+			this.displayStyle = HUDMessage.MessageStyle.NONE;
+			Singleton<HUDMessage>.Instance.FinishMessage (this.messageId);
+		}
+	}
+
+	// Token: 0x060019D6 RID: 6614 RVA: 0x000248BA File Offset: 0x00022ABA
+	public void Pause (bool isPause) {
+		this.tweenPosition.IsPause = isPause;
+		this.tweenAlpha.IsPause = isPause;
+	}
+
+	// Token: 0x04002E48 RID: 11848
+	[SerializeField]
+	private GameObject parentGameObject;
+
+	// Token: 0x04002E49 RID: 11849
+	private Transform myTransform;
+
+	// Token: 0x04002E4A RID: 11850
+	private UILabel label;
+
+	// Token: 0x04002E4B RID: 11851
+	private UIWidget uiWidget;
+
+	// Token: 0x04002E4C RID: 11852
+	private TweenPosition tweenPosition;
+
+	// Token: 0x04002E4D RID: 11853
+	private TweenAlpha tweenAlpha;
+
+	// Token: 0x04002E4E RID: 11854
+	private UIFollowTarget follower;
+
+	// Token: 0x04002E4F RID: 11855
+	[SerializeField]
+	private HUDMessage.MessageStyle displayStyle;
+
+	// Token: 0x04002E50 RID: 11856
+	private float tweenPositionDuration;
+
+	// Token: 0x04002E51 RID: 11857
+	private float tweenAlphaDuration;
+
+	// Token: 0x04002E52 RID: 11858
+	[SerializeField]
+	private byte messageId;
+
+	// Token: 0x04002E53 RID: 11859
+	private float countdown;
+
+	// Token: 0x04002E54 RID: 11860
+	private bool isInitialized;
+}
---
+++ ChangeSFX_Rush.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using Assets.Scripts.Common;
+using UnityEngine;
+
+// Token: 0x02000356 RID: 854
+public class SFX_Rush
+{
+	// Token: 0x060014D2 RID: 5330 RVA: 0x00150294 File Offset: 0x0014E494
+	public SFX_Rush()
+	{
+		this.rush_type = FF9StateSystem.Battle.isRandomEncounter;
+		this.rot = 0f;
+		this.rotInc = 0.03926991f;
+		this.subCol = 0f;
+		this.scale = 1f;
+		if (!this.rush_type)
+		{
+			this.addCol = 0.1f;
+			this.addColDec = 0.002f;
+			this.subColDec = 0.0008f;
+			this.scaleAdd = -0.008f;
+		}
+		else
+		{
+			this.addCol = 0.1f;
+			this.addColDec = 0.002f;
+			this.subColDec = 0.0008f;
+			this.scaleAdd = 0.006f;
+		}
+		if ((double)UnityEngine.Random.value > 0.5)
+		{
+			this.rotInc *= -1f;
+		}
+		Rect screenSize = SFX_Rush.GetScreenSize();
+		this.texture = new RenderTexture[2];
+		GL.PushMatrix();
+		GL.LoadIdentity();
+		Matrix4x4 mat = Matrix4x4.Scale(new Vector3(1f, 2f, 1f));
+		GL.MultMatrix(mat);
+		GL.Viewport(new Rect(0f, 0f, (float)Screen.width, (float)Screen.height));
+		for (int i = 0; i < 2; i++)
+		{
+			this.texture[i] = new RenderTexture((int)screenSize.width, (int)screenSize.height, 0, RenderTextureFormat.ARGB32);
+			this.texture[i].enableRandomWrite = false;
+			this.texture[i].wrapMode = TextureWrapMode.Clamp;
+			this.texture[i].filterMode = FilterMode.Point;
+			this.texture[i].Create();
+			this.texture[i].name = "Rush_RT_" + i;
+			Graphics.Blit(SFX_Rush.result, this.texture[i]);
+		}
+		GL.PopMatrix();
+		SceneDirector.FF9Wipe_FadeOutEx(5);
+	}
+
+	// Token: 0x060014D4 RID: 5332 RVA: 0x00150478 File Offset: 0x0014E678
+	public void ReleaseRenderTarget()
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			if (this.texture[i] != null)
+			{
+				this.texture[i].Release();
+				this.texture[i] = null;
+			}
+		}
+	}
+
+	// Token: 0x060014D5 RID: 5333 RVA: 0x001504C0 File Offset: 0x0014E6C0
+	public bool update() // TehMight
+	{
+		int num = this.rush_seq;
+		checked
+		{
+			this.rush_seq = num + 1;
+			if (num > BattleHUD.BattleSwirl + 5)
+			{
+				return true;
+			}
+			if (this.rush_seq > BattleHUD.BattleSwirl)
+			{
+				SceneDirector.ServiceFade();
+			}
+			this.isUpdate = true;
+			return false;
+		}
+	}
+
+	// Token: 0x060014D6 RID: 5334 RVA: 0x00150504 File Offset: 0x0014E704
+	public void PostProcess(RenderTexture src, RenderTexture dest)
+	{
+		int num = 0;
+		int num2 = 1;
+		if (this.isUpdate)
+		{
+			this.isUpdate = false;
+			Material material = new Material(Shader.Find("SFX_RUSH_SUB"));
+			material.SetVector("_Param", new Vector4(this.rot, this.scale, this.subCol, 0f));
+			Graphics.Blit(this.texture[num], this.texture[num2], material);
+			Material material2 = new Material(Shader.Find("SFX_RUSH_ADD"));
+			material2.SetVector("_Center", new Vector4(SFX_Rush.px, SFX_Rush.py, 0f, 0f));
+			material2.SetVector("_Param", new Vector4(this.rot, this.scale, this.addCol, 0f));
+			Graphics.Blit(this.texture[num], this.texture[num2], material2);
+			Graphics.Blit(this.texture[num2], this.texture[num]);
+			if ((this.rush_seq & 1) != 0)
+			{
+				SFX_Rush.px += ((SFX_Rush.px >= 0.5f) ? -0.009375f : 0.009375f);
+			}
+			if ((this.rush_seq & 2) != 0)
+			{
+				SFX_Rush.py += ((SFX_Rush.py >= 0.5f) ? -0.0133928573f : 0.0133928573f);
+			}
+			if (!this.rush_type)
+			{
+				float num3 = (float)Math.Sin((double)this.rush_seq * 14.0 * 3.1415926535897931 / 180.0);
+				SFX_Rush.px += num3 * (0.5f - SFX_Rush.px);
+				SFX_Rush.py += num3 * (0.5f - SFX_Rush.py);
+			}
+			if (!this.rush_type && this.rush_seq >= 16)
+			{
+				this.addCol -= this.addColDec;
+				if (this.addCol < 0f)
+				{
+					this.addCol = 0f;
+				}
+			}
+			if (this.rush_seq >= 1)
+			{
+				this.subCol += this.subColDec;
+				if (this.subCol > 1f)
+				{
+					this.subCol = 1f;
+				}
+			}
+			this.rot += this.rotInc;
+			this.scale += this.scaleAdd;
+		}
+		Graphics.Blit(this.texture[num], dest);
+	}
+
+	// Token: 0x060014D7 RID: 5335 RVA: 0x00150784 File Offset: 0x0014E984
+	public static Rect GetScreenSize()
+	{
+		Vector2 vector = new Vector2(320f, 224f);
+		if (PersistenSingleton<SceneDirector>.Instance.CurrentScene == "BattleMap" || PersistenSingleton<SceneDirector>.Instance.CurrentScene == "BattleMapDebug")
+		{
+			vector = new Vector2(320f, 220f);
+		}
+		float num = Mathf.Min((float)Screen.width / vector.x, (float)Screen.height / vector.y);
+		Rect rect = default(Rect);
+		rect.width = vector.x * num;
+		rect.height = vector.y * num;
+		rect.x = ((float)Screen.width - rect.width) * 0.5f;
+		rect.y = ((float)Screen.height - rect.height) * 0.5f;
+		return rect;
+	}
+
+	// Token: 0x060014D8 RID: 5336 RVA: 0x00150868 File Offset: 0x0014EA68
+	public static void CreateScreen()
+	{
+		Rect screenSize = SFX_Rush.GetScreenSize();
+		int num = (int)screenSize.x;
+		int num2 = (int)screenSize.y;
+		int num3 = (int)screenSize.width;
+		int num4 = (int)screenSize.height;
+		SFX_Rush.result = new Texture2D((int)screenSize.width, (int)screenSize.height, TextureFormat.ARGB32, false);
+		Color color = new Color(0f, 0f, 0f, 0f);
+		for (int i = 0; i < num3; i++)
+		{
+			SFX_Rush.result.SetPixel(i, 0, color);
+			SFX_Rush.result.SetPixel(i, num4 - 1, color);
+		}
+		for (int j = 1; j < num4 - 1; j++)
+		{
+			SFX_Rush.result.SetPixel(0, j, color);
+			SFX_Rush.result.SetPixel(num3 - 1, j, color);
+		}
+		SFX_Rush.result.ReadPixels(new Rect((float)(num + 1), (float)(num2 + 1), (float)(num3 - 2), (float)(num4 - 2)), 1, 1, false);
+		SFX_Rush.result.Apply();
+	}
+
+	// Token: 0x060014D9 RID: 5337 RVA: 0x00150974 File Offset: 0x0014EB74
+	public static void SetCenterPosition(int type)
+	{
+		if (type != 0)
+		{
+			if (type != 1)
+			{
+				SFX_Rush.px = 0.5f;
+				SFX_Rush.py = 0.5f;
+			}
+			else if (ff9.w_moveActorPtr != null)
+			{
+				Vector3 pos = ff9.w_moveActorPtr.pos;
+				Camera w_frameCameraPtr = ff9.w_frameCameraPtr;
+				Vector3 vector = w_frameCameraPtr.WorldToScreenPoint(pos);
+				vector.x /= (float)w_frameCameraPtr.pixelWidth;
+				vector.y /= (float)w_frameCameraPtr.pixelHeight;
+				SFX_Rush.px = vector.x;
+				SFX_Rush.py = vector.y;
+				global::Debug.Log(string.Concat(new object[]
+				{
+					"px : ",
+					SFX_Rush.px,
+					" , py : ",
+					SFX_Rush.py
+				}));
+			}
+			else
+			{
+				SFX_Rush.px = 0.5f;
+				SFX_Rush.py = 0.5f;
+			}
+		}
+		else
+		{
+			Obj objUID = PersistenSingleton<EventEngine>.Instance.GetObjUID(250);
+			if (objUID != null && objUID.cid == 4)
+			{
+				PosObj posObj = (PosObj)objUID;
+				FieldMap fieldmap = PersistenSingleton<EventEngine>.Instance.fieldmap;
+				Camera mainCamera = fieldmap.GetMainCamera();
+				BGCAM_DEF currentBgCamera = fieldmap.GetCurrentBgCamera();
+				Vector3 vertex = new Vector3(posObj.pos[0], posObj.pos[1], posObj.pos[2]);
+				Vector3 position = PSX.CalculateGTE_RTPT(vertex, Matrix4x4.identity, currentBgCamera.GetMatrixRT(), currentBgCamera.GetViewDistance(), fieldmap.GetProjectionOffset());
+				position = mainCamera.WorldToScreenPoint(position);
+				position.x /= (float)mainCamera.pixelWidth;
+				position.y /= (float)mainCamera.pixelHeight;
+				SFX_Rush.px = position.x;
+				SFX_Rush.py = position.y;
+				global::Debug.Log(string.Concat(new object[]
+				{
+					"px : ",
+					SFX_Rush.px,
+					" , py : ",
+					SFX_Rush.py
+				}));
+			}
+			else
+			{
+				SFX_Rush.px = 0.5f;
+				SFX_Rush.py = 0.5f;
+			}
+		}
+	}
+
+	// Token: 0x04002845 RID: 10309
+	private const int RushParamLastFrame = 120;
+
+	// Token: 0x04002846 RID: 10310
+	private const int StartFadeOutFrame = 115;
+
+	// Token: 0x04002847 RID: 10311
+	private bool rush_type = true;
+
+	// Token: 0x04002848 RID: 10312
+	private int rush_seq;
+
+	// Token: 0x04002849 RID: 10313
+	private static float px;
+
+	// Token: 0x0400284A RID: 10314
+	private static float py;
+
+	// Token: 0x0400284B RID: 10315
+	private RenderTexture[] texture;
+
+	// Token: 0x0400284C RID: 10316
+	private float addCol;
+
+	// Token: 0x0400284D RID: 10317
+	private float addColDec;
+
+	// Token: 0x0400284E RID: 10318
+	private float subCol;
+
+	// Token: 0x0400284F RID: 10319
+	private float subColDec;
+
+	// Token: 0x04002850 RID: 10320
+	private float rot;
+
+	// Token: 0x04002851 RID: 10321
+	private float rotInc;
+
+	// Token: 0x04002852 RID: 10322
+	private float scale;
+
+	// Token: 0x04002853 RID: 10323
+	private float scaleAdd;
+
+	// Token: 0x04002854 RID: 10324
+	private bool flip;
+
+	// Token: 0x04002855 RID: 10325
+	private bool isUpdate;
+
+	// Token: 0x04002856 RID: 10326
+	private static Texture2D result;
+}
